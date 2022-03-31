2e339595a49acbdb568955d3439c9f091b508554
Matt Bogenberger <mattbogenberger@gmail.com>
2022-03-30 22:13:59 -0400

    variables and methods

---
+++ python/script.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+# Variables and Methods
+quote = "I just took a DNA test, turns out I'm 100% that bitch."
+print(quote.upper()) # uppercase
+print(quote.lower()) # lowercase
+print(quote.title()) # title case
+
+print(len(quote))
+
+name = "Matt" #string
+age = 30 #int
+gpa = 3.7 #float
+
+print(int(age))
+print(int(30.9))
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+age += 1
+print(age)
+
+birthday = 1
+age += birthday
+print(age)
