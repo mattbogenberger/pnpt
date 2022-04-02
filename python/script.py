155bb13fd6308a3f455daa39f587657633229e58
Matt Bogenberger <mattbogenberger@gmail.com>
2022-04-02 07:27:46 -0400

    Conditional Statements

--- python/script.py
+++ python/script.py
@@ -95,3 +95,29 @@ test_or2 = (7 > 5) or (5 > 7) # True
 
 test_not = not True # False
 # print(test_not)
+
+nl()
+# Conditional statements
+def drink(money):
+    if money >= 2:
+        return "You've got yourself a drink!"
+    else:
+        return "NO drink for you!"
+
+print(drink(3))
+print(drink(1))
+
+def alcohol(age,money):
+    if (age >= 21) and (money >= 5):
+        return "We're getting a drink!"
+    elif (age >= 21 ) and (money < 5):
+        return "Come back with more money."
+    elif (age < 21) and (money >= 5):
+        return "Nice try, kid!"
+    else:
+        return "You're too poor and too young."
+
+print(alcohol(21,5))
+print(alcohol(21,4))
+print(alcohol(20,5))
+print(alcohol(20,4))
