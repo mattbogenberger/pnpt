515b41f1b498df434d49b8f9a0d5adbc7045bc80
Matt Bogenberger <mattbogenberger@gmail.com>
2022-04-07 08:34:07 -0400

    Dictionaries

--- python/new-script.py
+++ python/new-script.py
@@ -31,3 +31,21 @@ print(letter.lower() in word.lower()) # Improved
 
 movie = "The Hangover"
 print("My favorite movie is {}.".format(movie))
+
+# Dictionaries - key-value pairs {}
+drinks = {"White Russian": 7, "Old Fashioned": 10, "Lemon Drop": 8} # drink is key, price is value
+print(drinks)
+
+employees = {"Finance": ["Bob", "Linda", "Tina"], "IT": ["Gene", "Louise", "Teddy"], "HR": ["Jimmy Jr.", "Mort"]}
+print(employees)
+
+employees['Legal'] = ["Mr. Frond"] # add new key-value pair
+print(employees)
+
+employees.update({"Sales": ["Andie", "Ollie"]}) # add new key-value pair
+print(employees)
+
+drinks['White Russian'] = 8
+print(drinks)
+
+print(drinks.get("White Russian"))
